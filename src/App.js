be6259afb63dc9000970d7609c908dbab81628b2
Charlie Jarvis <csjarvis43@gmail.com>
2022-10-27 13:11:17 -0600

    initial routes done

--- src/App.js
+++ src/App.js
@@ -1,24 +1,28 @@
-import logo from './logo.svg';
+import { HashRouter as Router, Routes, Route } from "react-router-dom";
 import './App.css';
+import About from './components/About';
+import NavBar from './components/NavBar';
+import Projects from './components/Projects';
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <Router>
+      <NavBar/>
+      <Routes>
+        <Route 
+          path="/"
+          element={
+            <Projects />
+          }
+        />
+        <Route 
+          path="/about"
+          element={
+            <About />
+          }
+        />
+      </Routes>
+    </Router>
   );
 }
 
